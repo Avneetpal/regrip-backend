0f6274be87140d1337b7afb77108953e62380c99
AVNEET PAL <127130851+Avneetpal@users.noreply.github.com>
2026-02-13 11:03:07 +0530

    Add files via upload

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    port: process.env.MYSQLPORT || 3306,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
